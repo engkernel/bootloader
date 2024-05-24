cd8d8a968abf188214d2d3a0c35585f447735ed7
engkernel <engkernel@gmail.com>
2024-05-24 12:07:51 +0000

    revise the printing method

--- boot.asm
+++ boot.asm
@@ -1,20 +1,28 @@
 ; boot.asm
-	mov ax, 0x07c0
-	mov ds, ax
+ORG 0x7c00
+BITS 16
 
+_start:
 	mov si, msg
-	cld
-ch_loop: 
-	lodsb
-	or al, al ; zero=end of str
-	jz hang
-	mov ah, 0x0E
-	mov bh, 0
-	int 0x10
-	jmp ch_loop
-hang:
-	jmp hang
+	call print
+	jmp $
 
-msg db 'Hello World', 13, 10, 0
-times 510-($-$$) db 0 ; fill up to 510 byte
-dw 0xAA55
+print:
+	mov bx, 0
+.loop:
+	lodsb	
+	or al, al
+	jz .done
+	call putchar
+	jmp .loop
+.done:
+	ret
+
+putchar:
+	mov ah, 0eh
+	int 0x10
+	ret
+
+msg	db 'Hello World', 0
+times 510-($-$$) db 0 ; fill to 510 bytes with zero
+dw 0xAA55 ; last two byte, depened on little-endian or big-endian, now we have 512 bytes filled.
