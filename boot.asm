2db272b21af2b7a1454d3bad640bbf248fa49c79
engkernel <engkernel@gmail.com>
2024-05-25 18:01:54 +0000

    03-enter protected mode, print with video memory

--- boot.asm
+++ boot.asm
@@ -2,19 +2,20 @@
 ORG 0x7c00
 BITS 16
 
-CODE_SEGMENT equ gdt_code - gdt_start
-DATA_SEGMENT equ gdt_data - gdt_start
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
-	cli
 	call enable_A20
+	cli
 	lgdt [gdt_descriptor] ; load gdt
 	mov eax, cr0
 	or  eax, 0x1 ; set 32-bit mode in cr0
 	mov cr0, eax
-	jmp $
+	jmp CODE_SEG:load32 ; long jump code segment
 
 enable_A20:
+	; Fast gate enable A20 line
 	in al, 0x92
 	or al, 2
 	out 0x92, al
@@ -24,6 +25,8 @@ gdt_start:
 	; 8 null bytes 
 	dd 0x0 ; 4 bytes
 	dd 0x0 ; 4 bytes
+	; we code just simply write
+	; dq 0x0 
 
 ; GDT descriptor code segment
 gdt_code:
@@ -47,6 +50,31 @@ gdt_end:
 gdt_descriptor:
 	dw gdt_end - gdt_start - 1
 	dd gdt_start
+
+bits 32
+load32:
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	mov esi, hello
+	mov ebx, 0xb8000
+
+.loop:
+	lodsb
+	or al, al
+	or eax, 0x0100
+	mov word [ebx], ax
+	add ebx, 2
+	jmp .loop
+halt:
+	cli
+	hlt
+
+hello: db "Hello World!", 0
 
 times 510-($-$$) db 0 ; fill to 510 bytes with zero
 dw 0xAA55 ; last two byte, depened on little-endian or big-endian, now we have 512 bytes filled.
